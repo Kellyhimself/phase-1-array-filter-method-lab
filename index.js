c001569b3a9bdc60f2f4f9198c11d0e1111e4829
Kitui Kelly <kituikelly@gmail.com>
2024-03-26 19:20:50 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(driversArray, string) {
+  return driversArray.filter((driver) => {
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+}
+function fuzzyMatch(driversArray, string) {
+  return driversArray.filter((driver) => {
+    return driver.startsWith(string);
+  });
+}
+function matchName(driverObject, string) {
+  return driverObject.filter((driver) => {
+    return driver.name === string;
+  });
+}
